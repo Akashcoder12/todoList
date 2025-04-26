c0e7a5721526f6e02f495ca5063385bb7cfd410b
Akash gupta <www.akashgupta45@gmail.com>
2025-04-26 21:05:43 +0530

    addtask and delete task added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import './App.css';
+export default function App() {
+  const [task, setTask] = useState('');
+  const [taskList, setTaskList] = useState([]);
 
-function App() {
-  const [count, setCount] = useState(0)
+  const AddTask = () => {
+    if (task.trim()) {
+      setTaskList([...taskList, task]);
+      setTask('');
+    }
+  }
+
+  const RemoveTask = (indexToRemove) => {
+    setTaskList(taskList.filter((task, index) => index !== indexToRemove));
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1>Todo List</h1>
+      <input 
+        type="text"
+        placeholder="enter task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)} 
+      />
+      
+      <button onClick={AddTask}>AddTask</button>
+
+      <div className="display">
+        <ul>
+          {
+            taskList.map((task, index) => (
+              <li key={index}>
+                {task}
+                <button onClick={() => RemoveTask(index)}>delete</button>
+              </li>
+            ))
+          }
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
-
-export default App
